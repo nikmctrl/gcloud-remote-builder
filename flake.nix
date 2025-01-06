b3668be81f3b13b206fc6fc3d95fe7fa51690985
Nikolai Schimke <schimkeNikolai@gmail.com>
2025-01-06 22:26:22 +0000

    Enhance Nix flake and NixOS configurations with new dependencies and improved structure
    - Added 'deploy-rs' and 'flake-compat' as new dependencies in flake.lock for enhanced deployment capabilities.
    - Updated flake.nix to include 'deploy-rs' in the inputs and configured deployment nodes for NixOS and Home Manager.
    - Refined configuration.nix to integrate Sops for secure password management and updated user settings.
    - Improved formatting and structure across configuration files for better readability and maintainability.

--- flake.nix
+++ flake.nix
@@ -15,6 +15,9 @@
 
     # flake-utils
     flake-utils.url = "github:numtide/flake-utils";
+
+    # deploy-rs
+    deploy-rs.url = "github:serokell/deploy-rs";
   };
 
   outputs = {
@@ -23,13 +26,15 @@
     home-manager,
     sops-nix,
     flake-utils,
+    deploy-rs,
     ...
   } @ inputs: let
     inherit (self) outputs;
   in
-    flake-utils.lib.eachDefaultSystem (system: let pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShell = import ./shell.nix { inherit pkgs; };
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = import ./shell.nix {inherit pkgs;};
 
       formatter = pkgs.alejandra;
     })
@@ -37,14 +42,33 @@
       # Available through 'nixos-rebuild --flake .#instance-20250106-172607'
       nixosConfigurations = {
         instance-20250106-172607 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
           specialArgs = {inherit inputs outputs;};
-          # homeManager.extraSpecialArgs = {inherit inputs outputs;};
           modules = [
             ./nixos/configuration.nix
-
             sops-nix.nixosModules.sops
           ];
         };
+      };
+
+      deploy.nodes = {
+        instance-20250106-172607 = {
+          hostname = "instance-20250106-172607";
+          profiles = {
+            system = {
+            user = "root";
+              path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.instance-20250106-172607;
+            };
+            home = {
+              user = "nikolai";
+              path = deploy-rs.lib.x86_64-linux.activate.home-manager self.homeManagerConfigurations.nikolai;
+            };
+          };
+        };
+
+        checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+
+
       };
     };
 }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,6 +18,7 @@
 
     ./hardware-configuration.nix
     ./hm.nix
+    # inputs.sops-nix.nixosModules.sops
   ];
 
   nixpkgs = {
@@ -51,11 +52,21 @@
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
+  sops.defaultSopsFile = ../secrets/passwords.yaml;
 
+  sops.age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+  # This is using an age key that is expected to already be in the filesystem
+  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
+  # This will generate a new key if the key specified above does not exist
+  sops.age.generateKey = true;
+
+  sops.secrets.linux = {};
+
+  sops.secrets.linux.neededForUsers = true;
 
   boot.tmp.cleanOnBoot = true;
   zramSwap.enable = true;
-  networking.domain = "europe-west2-a.c.nix-ci-446719.internal"; 
+  networking.domain = "europe-west2-a.c.nix-ci-446719.internal";
   networking.hostName = "instance-20250106-172607";
 
   users.users = {
@@ -63,7 +74,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH7zhSffzoHpHVr8/TDJnhzvzDAWkS8GZO9udQaJ+ftl vii@nikbook"
     ];
     nikolai = {
-      hashedPasswordFile = sops.secrets."linux/password";
+      hashedPasswordFile = config.sops.secrets.linux.path;
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH7zhSffzoHpHVr8/TDJnhzvzDAWkS8GZO9udQaJ+ftl vii@nikbook"
